935c3ce936362717cb4458936e6ace7ce247aadd
foltysM <michal.foltys0@gmail.com>
2020-04-28 13:26:35 +0200

    Working threshold and morph operations for black cards

--- CardsRecognition/CardsRecognition.cpp
+++ CardsRecognition/CardsRecognition.cpp
@@ -1,17 +1,28 @@
 ﻿#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <stdlib.h>
+
+using namespace std;
+using namespace cv;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	Mat img = imread("test.jpg", 0);
+	namedWindow("image", WINDOW_NORMAL);
+	imshow("image", img);
+	pyrDown(img, img);
+	threshold(img, img, 60, 255, THRESH_BINARY); //WORKS ONLY WITH BLACK CARDS
+	//adaptiveThreshold(img, img, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY, 101, 1);
+	namedWindow("After threshold", WINDOW_NORMAL);
+	imshow("After threshold", img);
+	Mat structuring_element(3, 3, CV_8U, Scalar(1));
+	Mat closed = img.clone();
+	morphologyEx(img, closed, MORPH_CLOSE, structuring_element);
+	morphologyEx(closed, img, MORPH_CLOSE, structuring_element);
+	namedWindow("After morph", WINDOW_NORMAL);
+	imshow("After morph", img);
+
+	waitKey(0);
+	return 0;
 }
-
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
-
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
